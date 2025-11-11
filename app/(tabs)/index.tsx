0d28ccb97ec06f2a1db7bbbfe2c204ea81fb12bb
wawanneutron <wawanneutron1331@gmail.com>
2025-11-11 22:16:15 +0700

    feat: show list data movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,87 @@
-import { Text, View } from 'react-native'
+import SearchBar from '@/components/SearchBar'
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api'
+import useFetch from '@/services/useFetch'
+import { useRouter } from 'expo-router'
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View
+} from 'react-native'
 
 export default function Index() {
+  const router = useRouter()
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+  } = useFetch<Movie[]>(() =>
+    fetchMovies({
+      query: ''
+    })
+  )
+
   return (
-    <View className="px-6 flex-1 justify-center items-center">
-      <Text className="text-4xl text-dark-200 font-bold">Welcome Wawan!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <>
+            <Text className="text-white text-center text-xl font-bold">
+              {moviesError?.message}
+            </Text>
+            <Text className="text-white text-center text-xs mt-4">
+              {moviesError?.stack}
+            </Text>
+          </>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder="Search for a movie"
+            />
+
+            <Text className="text-lg text-white font-bold mt-5 mb-3">
+              Latest Movies
+            </Text>
+
+            <FlatList
+              data={movies}
+              renderItem={({ item }) => (
+                <Text className="text-white text-sm">{item.title}</Text>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: 'flex-start',
+                gap: 20,
+                paddingRight: 5,
+                marginBottom: 15
+              }}
+              scrollEnabled={false}
+              className="mt-2 pb-32"
+            />
+          </View>
+        )}
+      </ScrollView>
     </View>
   )
 }
